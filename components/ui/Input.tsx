e32b9dbbee3c42ec9e372cf1f56a8cf9b7e98261
kisshan26 <ks492013@outlook.com>
2023-05-17 17:41:23 +0530

    corrected focus for input feild

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -8,6 +8,7 @@ export default function Input({ className, ...props }: InputProps) {
     return (
         <input
             {...props}
-            className={cn(['border-solid border-2 outline-2 px-2 py-1 border-opacity-50 rounded-md border-white text-white focus-visible:border-opacity-0 font-saver text-xl focus-visible:outline-pink focus-visible:outline-main bg-black transition-all', className])} />
+            className={cn(
+                ['border-solid border-2 outline-2 px-2 py-1 border-opacity-50 rounded-md border-white text-white focus-visible:border-opacity-0 font-saver text-xl focus-visible:outline-pink bg-black transition-all', className])} />
     )
 }
